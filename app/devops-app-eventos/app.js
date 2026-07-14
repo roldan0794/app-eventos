dbf743ac00ad7da35640902617c283999226b57b
roldan0794 <roldanrodz94@gmail.com>
2026-07-14 09:45:20 +0000

    feat: evento 1 modificado

--- app/devops-app-eventos/app.js
+++ app/devops-app-eventos/app.js
@@ -5,7 +5,7 @@ const PORT = process.env.APP_PORT || 3000;
 const ENTORN_NAME = process.env.NODE_ENV || 'Desarrollo';
 
 const eventos = [
-  {id: 1, nombre: "DevOps Academy 2026", fecha: "2026-07-17", lugar: "online" },
+  {id: 1, nombre: "DevOps Academy 2026", fecha: "2026-07-17", lugar: "Saltillo" },
   {id: 2, nombre: "Confenrencia de Azure 4", fecha: "2026-07-14", lugar: "Accenture facilities"},
   {id: 3, nombre: "Laboratorio de Azure 3", fecha: "2026-07-15", lugar: "online"},
   {id: 4, nombre: "Clase CI/CD", fecha: "2026-07-14", lugar: "Monterrey"}
