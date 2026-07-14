9583996c8be817d04c2be6e75ad066ae95862cd3
roldan0794 <roldanrodz94@gmail.com>
2026-07-14 07:12:36 +0000

    agregando carpetas

---
+++ app/devops-app-eventos/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.APP_PORT || 3000;
+const ENTORN_NAME = process.env.NODE_ENV || 'Desarrollo';
+
+const eventos = [
+  {id: 1, nombre: "DevOps Academy 2026", fecha: "2026-07-17", lugar: "online" },
+  {id: 2, nombre: "Confenrencia de Azure", fecha: "2026-07-14", lugar: "Accenture facilities"},
+  {id: 3, nombre: "Laboratorio de Azure", fecha: "2026-07-15", lugar: "online"}
+];
+
+app.get('/', (req, res) => {
+  res.json({
+    proyecto: "Proyecto DevOps: Sistema de gestión de eventos",
+    entorno: ENTORN_NAME,
+    estado_conexion_db: "(OK)",
+    total_eventos: eventos.length,
+    lista_eventos: eventos
+  });
+});
+
+app.listen(PORT, () => {
+  console.log('Servidor de Eventos corriendo en el puerto ${PORT} en entorno de ${ENTORN_NAME}');
+});
